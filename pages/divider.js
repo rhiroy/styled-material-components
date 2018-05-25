24be62da97a70cdf677d9490426b38fe412bd343
Rhiannon Le Parmentier <rhiannonleparmentier@gmail.com>
2018-05-25 11:01:14 -0500

    Added Code Sandbox to Divider Page

--- pages/divider.js
+++ pages/divider.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import { ThemeProvider, Divider } from '../src';
+import { width } from 'window-size';
 
-const DividerPageComponent = ({
-  className,
-}) => (
+const DividerPageComponent = ({ className }) => (
   <ThemeProvider>
     <div className={className}>
       <h1>Divider</h1>
       <section>
-        <h2>
-          Provides visual space between two areas
-        </h2>
+        <h2>Provides visual space between two areas</h2>
         <div>
           Dividers are full-width by default
           <Divider />
@@ -24,17 +21,32 @@ const DividerPageComponent = ({
           <Divider inset="30%" />
         </div>
       </section>
+      <Fragment>
+        <StyledIframe
+          src="https://codesandbox.io/embed/0w8q60rw0"
+          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+        />{' '}
+      </Fragment>
       <section>
         <h2>Dividers in lists</h2>
         <p>
-          See <Link href="/lists"><a>the lists page</a></Link> for examples
-            of dividers used in lists
+          See{' '}
+          <Link href="/lists">
+            <a>the lists page</a>
+          </Link>{' '}
+          for examples of dividers used in lists
         </p>
       </section>
     </div>
   </ThemeProvider>
 );
 
-export default styled(DividerPageComponent)`
-
+const StyledIframe = styled.iframe`
+  width: 100%;
+  height: 500px;
+  border: 0;
+  border-radius: 4px;
+  overflow: hidden;
 `;
+
+export default styled(DividerPageComponent)``;
